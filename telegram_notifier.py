ea71e8e87690b27207682d1f9aabb5285250634e
homeautomationdz <108092263+homeautomationdz@users.noreply.github.com>
2024-08-04 12:11:24 +0100

    Update telegram_notifier.py

--- telegram_notifier.py
+++ telegram_notifier.py
@@ -1,10 +1,24 @@
 from telegram import Bot
 from telegram.error import TelegramError
-import config3  # Adjust to your config file
+import config
+
+async def send_message_to_telegram(symbol, high_30h, low_30h, buy_volume, sell_volume, volume_difference, price_change):
+    bot = Bot(token=config.bot_token)
+    try:
+        message = f"Symbol: {symbol}\n"
+        message += f"New 30-hour High: {high_30h}\n"
+        message += f"New 30-hour Low: {low_30h}\n"
+        message += f"Last Candle 30 min Buy Volume: {buy_volume}\n"
+        message += f"Last Candle 30 min Sell Volume: {sell_volume}\n"
+        message += f"Volume Difference: {volume_difference}\n"
+        message += f"Price Change in 30 min: {price_change}%\n"
+        await bot.send_message(chat_id=config.chat_id, text=message)
+    except TelegramError as e:
+        print(f"Error sending message to Telegram: {e}")
 
 async def send_image_to_telegram(image_buffer):
-    bot = Bot(token=config3.bot_token)  # Use the bot token from config3.py
+    bot = Bot(token=config.bot_token)
     try:
-        await bot.send_photo(chat_id=config3.chat_id, photo=image_buffer)  # Send the image buffer directly
+        await bot.send_photo(chat_id=config.chat_id, photo=image_buffer)
     except TelegramError as e:
         print(f"Error sending image to Telegram: {e}")
