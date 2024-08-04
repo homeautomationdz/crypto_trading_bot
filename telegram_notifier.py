30f9b58969aeee3ab22aa8b0546575ecf8871422
homeautomationdz <108092263+homeautomationdz@users.noreply.github.com>
2024-08-04 14:02:40 +0100

    Update telegram_notifier.py

--- telegram_notifier.py
+++ telegram_notifier.py
@@ -1,24 +1,27 @@
 from telegram import Bot
 from telegram.error import TelegramError
 import config
+import logging
+import asyncio
 
-async def send_message_to_telegram(symbol, high_30h, low_30h, buy_volume, sell_volume, volume_difference, price_change):
+async def send_message_to_telegram(symbol, current_price, new_high, new_low, buy_volume, sell_volume, volume_difference, volume_btc, volume_percentage, support, resistance, distance_from_support, distance_from_resistance, price_change, image_buffer=None):
     bot = Bot(token=config.bot_token)
     try:
         message = f"Symbol: {symbol}\n"
-        message += f"New 30-hour High: {high_30h}\n"
-        message += f"New 30-hour Low: {low_30h}\n"
+        message += f"New 30-hour High: {new_high}\n"
+        message += f"New 30-hour Low: {new_low}\n"
         message += f"Last Candle 30 min Buy Volume: {buy_volume}\n"
         message += f"Last Candle 30 min Sell Volume: {sell_volume}\n"
         message += f"Volume Difference: {volume_difference}\n"
         message += f"Price Change in 30 min: {price_change}%\n"
-        await bot.send_message(chat_id=config.chat_id, text=message)
-    except TelegramError as e:
-        print(f"Error sending message to Telegram: {e}")
 
-async def send_image_to_telegram(image_buffer):
-    bot = Bot(token=config.bot_token)
-    try:
-        await bot.send_photo(chat_id=config.chat_id, photo=image_buffer)
+        # Send the message
+        await bot.send_message(chat_id=config.chat_id, text=message)
+
+        # If an image buffer is provided, send the plot
+        if image_buffer:
+            await bot.send_photo(chat_id=config.chat_id, photo=image_buffer)
+            logging.info(f"Plot sent to Telegram for symbol {symbol}")
+
     except TelegramError as e:
-        print(f"Error sending image to Telegram: {e}")
+        logging.error(f"Error sending message to Telegram for symbol {symbol}: {e}")
